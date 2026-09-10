8973da48e81d717f93ad3faf6f9358705b741e91
Fabrice SALVAIRE <fabrice.salvaire@orange.fr>
2026-09-10 15:55:27 +0200

    add pyi

---
+++ src/edarw/eda/kicad/SexpSymbols.pyi
@@ -0,0 +1,59 @@
+from sexpdata import Symbol
+
+AT: Symbol
+
+BACKGROUND: Symbol
+
+BOTTOM: Symbol
+
+COLOR: Symbol
+
+DEFAULT: Symbol
+
+EFFECTS: Symbol
+END: Symbol
+EXTENDS: Symbol
+
+FILL: Symbol
+FONT: Symbol
+
+GENERATOR: Symbol
+
+HIDE: Symbol
+
+ID: Symbol
+IN_BOM: Symbol
+ITALIC: Symbol
+
+JUSTIFY: Symbol
+
+KICAD_SYMBOL_LIB: Symbol
+
+LEFT: Symbol
+LENGTH: Symbol
+LINE: Symbol
+
+NAME: Symbol
+NO: Symbol
+NUMBER: Symbol
+
+ON_BOARD: Symbol
+
+PIN: Symbol
+PROPERTY: Symbol
+
+RECTANGLE: Symbol
+
+SIZE: Symbol
+START: Symbol
+STROKE: Symbol
+SYMBOL: Symbol
+
+TOP: Symbol
+TYPE: Symbol
+
+VERSION: Symbol
+
+WIDTH: Symbol
+
+YES: Symbol
