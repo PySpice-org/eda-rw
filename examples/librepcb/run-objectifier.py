cd510e7e806cd2d3df8b9f0990733e589c3fdec2
Fabrice SALVAIRE <fabrice.salvaire@orange.fr>
2026-09-15 12:25:44 +0200

    add objectifier script

--- examples/librepcb/run-objectifier.py
+++ examples/librepcb/run-objectifier.py
@@ -6,15 +6,13 @@
 #
 ####################################################################################################
 
-from collections.abc import Iterable, Iterator
 from pathlib import Path
 
 from rich import print
 from rich.console import Console
 
-from edarw.objectifier import Objectifier, SchemaNode, StringList
 from edarw.log import setup_logging
-from edarw.text_buffer import TextBuffer
+from edarw.objectifier import Objectifier, PythonModuleGenerator
 
 ####################################################################################################
 
@@ -24,136 +22,11 @@ console = Console()
 
 ####################################################################################################
 
-def simplify_child_types(child_types: StringList) -> StringList:
-    new_list: list[str] = []
-    prev = None
-    while True:
-        if prev is None:
-            prev = child_types.pop()
-            new_list.append(prev)
-        else:
-            _ = child_types.pop()
-            if _ != prev:
-                new_list.append(_)
-                prev = _
-            elif not new_list[-1].endswith('*'):
-                new_list[-1] += '*'
-        if not child_types:
-            break
-    return StringList(new_list)
-
-def find_common_part(child_patterns: list[list[str]]) -> list[str]:
-    match len(child_patterns):
-        case 0:
-            return []
-        case 1:
-            return child_patterns[0]
-        case _:
-            common = []
-            for i in range(min(len(_) for _ in child_patterns)):
-                items = list(set([_[i] for _ in child_patterns]))
-                if len(items) == 1:
-                    common.append(items[0])
-                else:
-                    break
-            return common
-
-####################################################################################################
-
 project_path = Path('../../librepcb-examples')
 # file_path = 'calidou/circuit/circuit.lp'
 # file_path = 'calidou/schematics/main/schematic.lp'
 file_path = 'can2usb/boards/default/board.lp'
 path = project_path / file_path
 objectifier = Objectifier(path)
-
-# objectifier.dump()
-
-objectifier.get_schema()
-# for path, node in SchemaNode.NODES.items():
-#     print(f"[blue]{path}[/]: {node}")
-
-buffer = TextBuffer()
-
-def make_class_name(node: SchemaNode) -> str:
-    return node.name.title().replace('_', '')
-
-def make_field_type(parent: SchemaNode, node: SchemaNode, is_list: bool) -> tuple[str, str]:
-    if len(node):
-        class_name = make_class_name(node)
-        if is_list:
-            return f'list[{class_name}]', ''
-        else:
-            return class_name, ''
-    else:
-        type_patterns = list(node.child_types)
-        if len(type_patterns) > 1:
-            # raise NameError('More than one type pattern for {node}')
-            return '...', '{type_patterns}'
-        types = type_patterns[0]
-        match len(types):
-            case 0:
-                # raise NameError('Empty type for {node}')
-                return '...', f'Empty type ! {type_patterns}'
-            case 1:
-                field_type = types[0]
-                if 'Symbol' in field_type:
-                    childs = [_.get_child(node.name) for _ in parent._instances]
-                    values = list(set(str(_.childs[0]) for _ in childs if _ is not None))
-                    if 'true' in values or 'false' in values:
-                        return 'bool', ''
-                    if values[0].count('-') == 4:
-                        return 'UUID', ''
-                    return 'str', str(values)
-                return field_type, ''
-            case _:
-                return f'tuple[{', '.join(types)}]', ''
-
-def make_class(node: SchemaNode) -> None:
-    global buffer
-    buffer.new_line()
-    buffer += '#'*100
-    buffer.new_line()
-    class_name = make_class_name(node)
-    buffer += f"class {class_name}(SexpWrapper):"
-    buffer.indent()
-    buffer += f"CAR = '{node.name}'"
-
-    child_map: dict[str, SchemaNode] = {child.name: child for child in node}
-
-    child_patterns = list(set(simplify_child_types(_) for _ in node.child_types))
-    # if not child_patterns or max(len(_) for _ in child_patterns) == 1:
-    #     continue
-    common = find_common_part(child_patterns)
-    for field in common:
-        try:
-            is_list = field.endswith('*')
-            if is_list:
-                field = field[:-1]
-            child = child_map[field]
-            field_type, comment = make_field_type(node, child, is_list)
-            if comment:
-                comment = f'  # {comment}'
-            buffer += f"{field}: {field_type}{comment}"
-        except KeyError:
-            buffer += f"# {field}:"
-    patterns = sorted(child_patterns, reverse=True, key=len)
-    for _ in patterns:
-        tail = _[len(common):]
-        if tail:
-            buffer += '# ' + str(tail)
-    for field in patterns[0][len(common):]:
-        if field.endswith('*'):
-            field = field[:-1]
-        child = child_map[field]
-        field_type, _ = make_field_type(node, child, True)
-        buffer += f"{field}: {field_type}"
-
-    buffer.dedent()
-
-def on_leave(node: SchemaNode) -> None:
-    if len(node) and node.name != '/':
-        make_class(node)
-
-SchemaNode.root().depth_first_search(on_leave=on_leave)  # ty: ignore[invalid-argument-type]
-print(buffer)
+generator = PythonModuleGenerator(objectifier)
+print(generator)
--- src/edarw/objectifier.py
+++ src/edarw/objectifier.py
@@ -22,6 +22,8 @@ import sexpdata as S
 from rich import print
 from sexpdata import Symbol
 
+from edarw.text_buffer import TextBuffer
+
 ####################################################################################################
 
 _module_logger = logging.getLogger(__name__)
@@ -103,6 +105,7 @@ class StringList:
 ####################################################################################################
 
 class TreeMixin:
+    """Base class to implement a node of a tree."""
 
     ##############################################
 
@@ -145,7 +148,6 @@ class TreeMixin:
             on_node: Callable[[TreeMixin], bool] | None = None,
             on_leaf: Callable[[TreeMixin], None] | None = None,
             on_leave: Callable[[TreeMixin], None] | None = None,
-            reverse: bool = False,
     ) -> None:
         go = True
         if on_node is not None:
@@ -164,6 +166,11 @@ class TreeMixin:
 ####################################################################################################
 
 class Node(TreeMixin):
+    """Class to store a Sexpr node `(node_name child1 chil2 ...)`.
+
+    `path` is the Sexpr stack `(node1 (node 1 ... (node_name]))`
+
+    """
 
     ##############################################
 
@@ -176,6 +183,14 @@ class Node(TreeMixin):
     @property
     def name(self) -> str:
         return self._path[-1]
+
+    @property
+    def parent_name(self) -> str:
+        return self._path[-2]
+
+    @property
+    def root_name(self) -> str:
+        return self._path[0]
 
     @property
     def path(self) -> list[str]:
@@ -203,6 +218,7 @@ class Node(TreeMixin):
     ##############################################
 
     def xpath(self, path: str) -> list[Node]:
+        """Return all the nodes with the given path from the current node."""
         # DEBUG = False
         # DEBUG = True
 
@@ -252,11 +268,13 @@ class Node(TreeMixin):
     ##############################################
 
     @property
-    def child_types(self) -> list:
+    def child_types(self) -> list[str]:
+        """Return the list of child's type name"""
         return list(type(_).__name__ for _ in self)
 
     @property
-    def child_types2(self) -> list:
+    def child_types2(self) -> list[str]:
+        """Return the list of child's type name or the Sexpr car (name) If child is a Node instance."""
         def child_type(child: Any) -> str:  # ruff: ignore[any-type]
             if isinstance(child, Node):
                 return child.name
@@ -265,7 +283,10 @@ class Node(TreeMixin):
 
         return list(child_type(_) for _ in self)
 
+    ##############################################
+
     def get_child(self, name: str) -> Node | None:
+        """Return the child having the given name or None if it is not found."""
         for child in self:
             if isinstance(child, Node) and child.name == name:
                 return child
@@ -274,6 +295,9 @@ class Node(TreeMixin):
 ####################################################################################################
 
 class SchemaNode(TreeMixin):
+    """Class to define a Sexpr node.
+
+    """
 
     # key is node path
     NODES: dict[str, SchemaNode] = {}
@@ -282,6 +306,7 @@ class SchemaNode(TreeMixin):
 
     @classmethod
     def get_node(cls, node: Node) -> SchemaNode:
+        """Return the SchemaNode for the given node."""
         if not cls.NODES:
             # add root
             cls.NODES['/'] = SchemaNode('/')
@@ -298,11 +323,17 @@ class SchemaNode(TreeMixin):
 
     @classmethod
     def root(cls) -> SchemaNode:
+        """Root of the tree"""
         return cls.NODES['/']
 
     ##############################################
 
     def __init__(self, name: str) -> None:
+        """Create a SchemNode.
+
+        `name` is the CAR of the sexpr.
+
+        """
         super().__init__()
         self._name = name
         self._instances = []
@@ -311,6 +342,7 @@ class SchemaNode(TreeMixin):
 
     @property
     def name(self) -> str:
+        """CAR of the sexpr"""
         return self._name
 
     ##############################################
@@ -327,14 +359,170 @@ class SchemaNode(TreeMixin):
 
     ##############################################
 
+    # register ?
     def link_instance(self, instance: Node) -> None:
+        """Link a `Node` instance."""
         self._instances.append(instance)
 
     ##############################################
 
     @property
     def child_types(self) -> set[StringList]:
+        """Return a set of the child types for the instances."""
         return set(StringList(_.child_types2) for _ in self._instances)
+
+####################################################################################################
+
+class PythonModuleGenerator:
+
+    ##############################################
+
+    @classmethod
+    def simplify_child_types(cls, child_types: StringList) -> StringList:
+        new_list: list[str] = []
+        prev = None
+        while True:
+            if prev is None:
+                prev = child_types.pop()
+                new_list.append(prev)
+            else:
+                _ = child_types.pop()
+                if _ != prev:
+                    new_list.append(_)
+                    prev = _
+                elif not new_list[-1].endswith('*'):
+                    new_list[-1] += '*'
+            if not child_types:
+                break
+        return StringList(new_list)
+
+    ##############################################
+
+    @classmethod
+    def find_common_part(cls, child_patterns: list[list[str]]) -> list[str]:
+        match len(child_patterns):
+            case 0:
+                return []
+            case 1:
+                return child_patterns[0]
+            case _:
+                common = []
+                for i in range(min(len(_) for _ in child_patterns)):
+                    items = list(set([_[i] for _ in child_patterns]))
+                    if len(items) == 1:
+                        common.append(items[0])
+                    else:
+                        break
+                return common
+
+    ##############################################
+
+    @classmethod
+    def make_class_name(cls, node: SchemaNode) -> str:
+        return node.name.title().replace('_', '')
+
+    ##############################################
+
+    @classmethod
+    def make_field_type(cls, parent: SchemaNode, node: SchemaNode, is_list: bool) -> tuple[str, str]:
+        if len(node):
+            class_name = cls.make_class_name(node)
+            if is_list:
+                return f'list[{class_name}]', ''
+            else:
+                return class_name, ''
+        else:
+            type_patterns = list(node.child_types)
+            if len(type_patterns) > 1:
+                # raise NameError('More than one type pattern for {node}')
+                return '...', '{type_patterns}'
+            types = type_patterns[0]
+            match len(types):
+                case 0:
+                    # raise NameError('Empty type for {node}')
+                    return '...', f'Empty type ! {type_patterns}'
+                case 1:
+                    field_type = types[0]
+                    if 'Symbol' in field_type:
+                        childs = [_.get_child(node.name) for _ in parent._instances]
+                        values = list(set(str(_.childs[0]) for _ in childs if _ is not None))
+                        if 'true' in values or 'false' in values:
+                            return 'bool', ''
+                        if values[0].count('-') == 4:
+                            return 'UUID', ''
+                        return 'str', str(values)
+                    return field_type, ''
+                case _:
+                    return f'tuple[{', '.join(types)}]', ''
+
+    ##############################################
+
+    def __init__(self, objectifier: Objectifier) -> None:
+        # Fixme: run once
+        objectifier.get_schema()
+        self._buffer: TextBuffer
+        self._make()
+
+    ##############################################
+
+    def __str__(self) -> str:
+        return str(self._buffer)
+
+    ##############################################
+
+    def _make_class(self, node: SchemaNode) -> None:
+        self._buffer.new_line()
+        self._buffer += '#' * 100
+        self._buffer.new_line()
+        class_name = self.make_class_name(node)
+        self._buffer += f"class {class_name}(SexpWrapper):"
+        self._buffer.indent()
+        self._buffer += f"CAR = '{node.name}'"
+
+        child_map: dict[str, SchemaNode] = {child.name: child for child in node}
+
+        child_patterns = list(set(self.simplify_child_types(_) for _ in node.child_types))
+        # if not child_patterns or max(len(_) for _ in child_patterns) == 1:
+        #     continue
+        common = self.find_common_part(child_patterns)
+        for field in common:
+            try:
+                is_list = field.endswith('*')
+                if is_list:
+                    field = field[:-1]
+                child = child_map[field]
+                field_type, comment = self.make_field_type(node, child, is_list)
+                if comment:
+                    comment = f'  # {comment}'
+                self._buffer += f"{field}: {field_type}{comment}"
+            except KeyError:
+                self._buffer += f"# {field}:"
+        patterns = sorted(child_patterns, reverse=True, key=len)
+        for _ in patterns:
+            tail = _[len(common):]
+            if tail:
+                self._buffer += '# ' + str(tail)
+        for field in patterns[0][len(common):]:
+            if field.endswith('*'):
+                field = field[:-1]
+            child = child_map[field]
+            field_type, _ = self.make_field_type(node, child, True)
+            self._buffer += f"{field}: {field_type}"
+
+        self._buffer.dedent()
+
+    ##############################################
+
+    def _make(self) -> None:
+        self._buffer = TextBuffer()
+
+        def make_callback(self: PythonModuleGenerator) -> Callable[[TreeMixin], None]:
+            def on_leave(node: SchemaNode) -> None:
+                if len(node) and node.name != '/':
+                    self._make_class(node)
+            return on_leave  # ty: ignore[invalid-return-type]
+
+        SchemaNode.root().depth_first_search(on_leave=make_callback(self))
 
 ####################################################################################################
 
